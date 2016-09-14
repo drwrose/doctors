0ca39cc95f43dc2732ed14ac9e203e12f1d34e89
David Rose <david@ddrose.com>
2016-09-14 10:25:26 -0700

    palettize emery faces

--- resources/make_palette.py
+++ resources/make_palette.py
@@ -1,6 +1,8 @@
 import PIL.Image
 import sys
 
+# First, define an image with a 64-color palette that corresponds
+# exactly to the Pebble Time's color space.
 pal = PIL.Image.new('RGB', (8, 8))
 for yi in range(8):
     for xi in range(8):
@@ -10,14 +12,22 @@ for yi in range(8):
         b = (pi >> 4) & 3
         pal.putpixel((xi, yi), (r * 0x55, b * 0x55, g * 0x55))
 pal = pal.convert("P", palette = PIL.Image.ADAPTIVE)
-pal.save('pal.png')
+#pal.save('palette_64.png')
 
 def make_palette(filename):
     im = PIL.Image.open(filename)
+    if im.mode != 'RGB':
+        print "skipping"
+        return
 
+    # First, convert it to the 64-color Pebble Time palette.
     im2 = im.quantize(palette = pal)
+
+    # Then, find the 16 most common colors in the resulting image.
     palette = im2.getpalette()
     colors = sorted(im2.getcolors(), reverse = True)
+
+    # And build a palette with just those 16 colors.
     pal2 = []
     for count, index in colors[:16]:
         index *= 3
@@ -26,8 +36,9 @@ def make_palette(filename):
         pal2 += palette[index : index + 3]
     im2.putpalette(pal2)
 
-    im = im.convert("P", palette = im2)
-    im.save('t.png')
+    # Now convert it again to that palette.
+    im = im.quantize(palette = im2)
+    im.save('palette/' + filename)
 
 for filename in sys.argv[1:]:
     print filename
